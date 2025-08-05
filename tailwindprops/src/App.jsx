85785cee08f4e89c1ecf40faf5baf166f97a31af
Deepanshu-K991 <deepanshukumar842@gmail.com>
2025-08-05 13:28:25 +0530

    props and others

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -4,11 +4,19 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+let myObj = {
+  username : "deepanshu",
+  age : 21
+}
 
+let myarr = [1,2,3,4,5,]
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
-      <Card />
+      <Card username="mychannel" btntext = "click here to know more" />
+      <Card username="other channel"  />
+      <Card username="third channel" btntext='click me' />
+
     </>
   )
 }
--- tailwindprops/src/components/Card.jsx
+++ tailwindprops/src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-function Card() {
+function Card( {username , btntext = "default"}  ) {
+  // console.log(props.username); /* this will become username only*/
   return (
     <div className="max-w-xs rounded-md shadow-md bg-black text-gray-100">
         <img
@@ -10,7 +11,9 @@ function Card() {
         />
         <div className="flex flex-col justify-between p-6 space-y-8">
           <div className="space-y-2">
-            <h2 className="text-3xl font-semibold tracking-wide">Lorem</h2>
+            <h2 className="text-3xl font-semibold tracking-wide"> 
+              {/* {props.username} */ username}
+            </h2>
             <p className="text-gray-400">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio tempora ipsum
               soluta amet corporis accusantium aliquid consectetur eaque!
@@ -20,7 +23,7 @@ function Card() {
             type="button"
             className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-gray-800 text-gray-200"
           >
-            Read more
+            {/* {props.btntext} */btntext }
           </button>
         </div>
       </div>

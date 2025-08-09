6eb97d568efb519b2c708ab9a1fa3809d881e318
Deepanshu-K991 <deepanshukumar842@gmail.com>
2025-08-09 14:54:14 +0530

    old changes

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -15,7 +15,7 @@ let myarr = [1,2,3,4,5,]
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
       <Card username="mychannel" btntext = "click here to know more" />
       <Card username="other channel"  />
-      <Card username="third channel" btntext='click me' />
+      <Card username="thirdo channel" btntext='click me' />
 
     </>
   )
